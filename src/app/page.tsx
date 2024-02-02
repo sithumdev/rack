ea730a5ca5662f20671622459432ee7436c44267
Sithum Basnayaka <sithum.dashantha@gmail.com>
2024-02-02 08:00:37 +0530

    mobile responsive dashboard and other pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { MostDemandingProduct, TopReleases } from "./_components";
 
 export default function Home() {
   return (
-    <section className="grid grid-cols-2 gap-2">
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-2">
       <MostDemandingProduct />
       <TopReleases />
     </section>
