ab8d386a45633874dd16aa7191cf861a05f388d6
Sithum Basnayaka <sithum.dashantha@gmail.com>
2024-02-02 08:39:42 +0530

    removed unused code

--- src/app/_components/top-releases.tsx
+++ src/app/_components/top-releases.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { BarChartCloud } from ".";
 import { ReportMode } from "../_lib/globals";
 import { IAnalyticsDemandingProduct } from "./bar-chart";
@@ -8,27 +8,6 @@ import { IAnalyticsDemandingProduct } from "./bar-chart";
 export default function TopReleases() {
   const [mode, setMode] = useState<ReportMode>(ReportMode.DAILY);
   const [analytics, setAnalytics] = useState<IAnalyticsDemandingProduct[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const formData = new FormData();
-
-      formData.append("mode", mode);
-
-      const response = await fetch("/api/analytics/release", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (response.status === 200) {
-        const data = await response.json();
-
-        if (data && Object.hasOwn(data, "reporting")) {
-          console.log(data.reporting);
-        }
-      }
-    })();
-  }, [mode]);
 
   return (
     <div>
